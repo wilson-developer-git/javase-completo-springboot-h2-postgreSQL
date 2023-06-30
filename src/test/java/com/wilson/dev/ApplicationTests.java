068f5f12d9c4953de0de38582f13652328f3e35a
Wilson <wilsonprogram@gmail.com>
2023-06-30 09:10:09 -0300

    Aula 298 - Criação do projeto

---
+++ src/test/java/com/wilson/dev/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
