60baf84160bbae199127a8842b7d69ed19ec2a88
Wilson <wilsonprogram@gmail.com>
2023-07-02 12:02:33 -0300

    Aula 302 -  Camada de serviço, registro de componentes

--- src/main/java/com/wilson/dev/controller/UserController.java
+++ src/main/java/com/wilson/dev/controller/UserController.java
@@ -1,20 +1,33 @@
 package com.wilson.dev.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wilson.dev.model.User;
+import com.wilson.dev.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "João da Silva", "joao@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping("/id/{userId}")
+	public ResponseEntity<User> findById(@PathVariable Long userId){
+		User user = userService.findById(userId);
+		return ResponseEntity.ok().body(user);
+	}
 }
---
+++ src/main/java/com/wilson/dev/service/UserService.java
@@ -0,0 +1,24 @@
+package com.wilson.dev.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wilson.dev.model.User;
+import com.wilson.dev.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long userId){
+		return userRepository.findById(userId).get();
+	}
+}
