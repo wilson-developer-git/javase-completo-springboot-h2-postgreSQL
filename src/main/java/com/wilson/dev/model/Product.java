5cc821aca0825571a694aad981864d06df12e9ec
Wilson <wilsonprogram@gmail.com>
2023-07-05 08:55:49 -0300

    Aula 308 - Associação muitos-para-muitos com JoinTable

--- src/main/java/com/wilson/dev/model/Product.java
+++ src/main/java/com/wilson/dev/model/Product.java
@@ -9,7 +9,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Transient;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity(name = "tb_product")
 public class Product implements Serializable{
@@ -23,7 +25,10 @@ public class Product implements Serializable{
 	private Double price;
 	private String imageUrl;
 	
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
