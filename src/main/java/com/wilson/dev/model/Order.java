d8f8cd7e42015d9f126d4d73ef0e07547e5de291
Wilson <wilsonprogram@gmail.com>
2023-07-06 09:48:05 -0300

    Aula 313 - Métodos subtotal e total

--- src/main/java/com/wilson/dev/model/Order.java
+++ src/main/java/com/wilson/dev/model/Order.java
@@ -97,6 +97,14 @@ public class Order implements Serializable{
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem orderItem : items) {
+			sum += orderItem.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
