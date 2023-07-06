b3d91795cedaec901ad27b813e1ee82f00ff6c21
Wilson <wilsonprogram@gmail.com>
2023-07-06 09:29:42 -0300

    Aula 312 - Entidade Payment, associação um para um

--- src/main/java/com/wilson/dev/config/TestConfig.java
+++ src/main/java/com/wilson/dev/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wilson.dev.model.Category;
 import com.wilson.dev.model.Order;
 import com.wilson.dev.model.OrderItem;
+import com.wilson.dev.model.Payment;
 import com.wilson.dev.model.Product;
 import com.wilson.dev.model.User;
 import com.wilson.dev.model.enums.OrderStatus;
@@ -76,6 +77,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
